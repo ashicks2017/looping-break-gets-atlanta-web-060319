3411d865ff7e28a079de31bb38d875c48483a075
aeisha simmons <ashicks2017@outlook.com>
2019-05-06 13:39:44 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,18 +1,10 @@
 
-#def levitation_quiz
-	#your code here
-#end
-
-def annoying 
-  loop do
-    puts "What's up, Doc ?"
-    answer = gets.chomp
-    break if answer == "STOP"
-    
-  end
-  
-  puts "Okay, Okay, jeez. I'll stop. Sorry"
-
+def levitation_quiz
+	loop do 
+	  puts "What is the spell that enact levitation?"
+	  answer = gets.chomp
+	  
+	  break if answer = "Wingardium Leviosa"
 end
 
-annoying
+puts "You passed the quiz!"
