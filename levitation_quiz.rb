e31d38cd6b4ed8c337233d2e701b9dede9d69b16
aeisha simmons <ashicks2017@outlook.com>
2019-05-06 13:20:50 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,4 +3,12 @@ def levitation_quiz
 	#your code here
 end
 
-
+def annoying 
+  loop do
+    puts "What's up, Doc ?"
+    answer = gets.chomp
+    break if answer == "STOP"
+    
+  end
+  
+  puts "Okay, Okay, jeez. I'll"
